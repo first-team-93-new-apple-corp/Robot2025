e4f1d53c6c316cf4cb307cac86323b2119a0caab
Driver2 <plexus@aasd.k12.wi.us>
2025-02-07 18:55:50 -0600

    Deleted unused file

--- src/main/java/frc/robot/subsystems/GrabberSubsystem.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.subsystems;
-
-import com.revrobotics.spark.SparkMax;
-
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-public class GrabberSubsystem extends SubsystemBase{
-    SparkMax grabber = new SparkMax(0, null);
-}
